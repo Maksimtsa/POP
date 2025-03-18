bca7667022673b2c44c38557c02287db7a51e3a6
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-03-18 09:07:17 +0100

    Create 1lessonAfter6Version.cs

---
+++ refaktoryzacja/1lessonAfter6Version.cs
@@ -0,0 +1,57 @@
+
+
+namespace refaktoryzacja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // konwersja dużych instrukcji if na Dictionary
+
+            string name = null;
+            if (name == null) name = "Domyślna wartość";
+
+            string[] names =  { "Katarzyna", "Franciszek", "Jan" };
+            var users = new Dictionary<int, string>();
+
+            int index = 1;
+            foreach(var userName in names)
+            {
+                users.Add(index++, userName);
+            }
+
+            Console.Write("Podaj ID użytkownika (1-3)");
+            if (int.TryParse(Console.ReadLine(), out int userId) && users.ContainsKey(userId))
+            {
+                string userName = users[userId];
+                string role = GetUserRole(userId);
+                Console.WriteLine($"Użytkownik: {userName}, rola użytkownika: {role}");
+            }
+            else
+            {
+                Console.WriteLine("Nieporawny numer roli");
+            }
+            Console.WriteLine("Podaj typ klienta (Silver, Gold, Platinum): ");
+            string customerType = Console.ReadLine();
+            int discount = GetDiscount(customerType);
+            Console.WriteLine($"Rabat dla klienta {customerType}: {discount}");
+        }
+
+        private static int GetDiscount(string? customerType)
+        {
+            if (customerType == "Silver") return 5;
+            if (customerType == "Gold") return 10;
+            if (customerType == "Platinum") return 15;
+            return 0;
+        }
+
+        // nowoczensy sposób switch
+        private static string GetUserRole(int userId) => userId switch
+        {
+            1 => "Administrator",
+            2 => "Moderator",
+            3 => "Użytkownik",
+            _ => "Nieznana rola",
+        };
+    }
+}
