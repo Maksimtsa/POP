755d3aa89bd03466d3869b0be2d5ac4c5df35db1
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-04-29 09:31:22 +0200

    Add files via upload

---
+++ Test/MyApp.Test/Services/UserServiceTest.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+using MyApp.Services;
+using MyApp.Models;
+
+namespace MyApp.Test.Services
+{
+    public class UserServicesTest
+    {
+        [Fact]
+        public void AddUser_ShouldAddUserSuccessfully()
+        {
+            // Arrange
+            var service = new UserServices();
+            var user = new User(1, "Franciszek Kowalski");
+
+            // Act
+            service.AddUser(user);
+
+            // Assert
+            var result = service.GetUserById(1);
+            Assert.NotNull(result);
+            Assert.Equal("Franciszek Kowalski", result.Name);
+        }
+        [Fact]
+        public void GetUserById_UserDoesNotExist_ShouldReturnNull()
+        {
+            //Arrange
+            var service = new UserServices();
+
+            //Act
+            var result = service.GetUserById(1000);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void RemoveUser_ShouldRemoveUserSuccessfully()
+        {
+            //Arrange
+            var service = new UserServices();
+            var user = new User(2, "Tomasz Nowak");
+            service.AddUser(user);
+
+            //Act
+            var removed = service.RemoveUsers(2);
+
+
+            //Assert
+            Assert.True(removed);
+            Assert.Null(service.GetUserById(2));
+        }
+
+        [Fact]
+        public void GetAllUsers_ShouldReturnUserSuccessfully()
+        {
+            //Arrange
+            var service = new UserServices();
+            service.AddUser(new User(1, "Franciszek"));
+
+            //Act
+            var removed = service.RemoveUsers(2);
+
+
+            //Assert
+            Assert.True(removed);
+            Assert.Null(service.GetUserById(2));
+        }
+    }
+}
---
+++ Test/MyApp.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApp.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
